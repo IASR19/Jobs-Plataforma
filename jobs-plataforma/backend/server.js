d1413d32eef3060354ebd9fc317e3285766da486
IGSR19 <itamar_augusto@ges.inatel.br>
2024-01-26 18:38:50 -0300

    BD :recycle:

--- jobs-plataforma/backend/server.js
+++ jobs-plataforma/backend/server.js
@@ -8,19 +8,38 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors()); // Adiciona o middleware CORS
 
-
 // Conectar ao MongoDB (substitua a URL pelo seu próprio cluster do MongoDB)
 mongoose.connect('mongodb+srv://itamar_asr:eu190899@cluster0.dx3mu2d.mongodb.net/JOBs?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Definir um modelo de usuário simples
-const User = mongoose.model('usuarios', { tipo: String });
+// Definir modelos de usuário para recrutadores e candidatos
+const recrutadorSchema = new mongoose.Schema({
+  tipo: String,
+  // Outros campos específicos de recrutadores
+});
+
+const candidatoSchema = new mongoose.Schema({
+  tipo: String,
+  // Outros campos específicos de candidatos
+});
+
+const Recrutador = mongoose.model('recrutadores', recrutadorSchema);
+const Candidato = mongoose.model('candidatos', candidatoSchema);
 
 // Rota para salvar a opção escolhida no banco de dados
 app.post('/api/saveOption', async (req, res) => {
   try {
     const { tipo } = req.body;
-    const user = new User({ tipo });
-    await user.save();
+    let usuario;
+
+    if (tipo === 'RECRUTADOR') {
+      usuario = new Recrutador({ tipo, /* outros campos específicos de recrutadores */ });
+    } else if (tipo === 'CANDIDATO') {
+      usuario = new Candidato({ tipo, /* outros campos específicos de candidatos */ });
+    } else {
+      return res.status(400).json({ error: 'Tipo de usuário inválido.' });
+    }
+
+    await usuario.save();
     res.status(201).json({ message: 'Opção salva com sucesso!' });
   } catch (error) {
     console.error(error);
